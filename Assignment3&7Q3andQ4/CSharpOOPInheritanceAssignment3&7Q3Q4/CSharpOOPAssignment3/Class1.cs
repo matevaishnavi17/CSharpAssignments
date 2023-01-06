99f859cf8a4f6f720cc7065e97ece3103bddc8a8
Vaishnavi Ganesh Mate <vaishnavi.ganesh-mate@capgemini.com>
2023-01-06 14:00:24 +0530

    Assignmnet 7 pushed

---
+++ Assignment3&7Q3andQ4/CSharpOOPInheritanceAssignment3&7Q3Q4/CSharpOOPAssignment3/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CSharpOOPAssignment3
+{
+    class Program
+    {
+        public static void SerializeObject(Object o)
+        {
+            FileStream fs = new FileStream(@"C:\Users\VGANESHM\source\repos\CSharpOOPInheritanceAssignment3\output.txt", FileMode.OpenOrCreate);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fs, o);
+            fs.Close();
+        }
+
+        public static void DeserializeObject()
+        {
+            FileStream fs = new FileStream(@"C:\Users\VGANESHM\source\repos\CSharpOOPInheritanceAssignment3\output.txt", FileMode.OpenOrCreate);
+            BinaryFormatter formatter = new BinaryFormatter();
+            Manager m1 = new Manager(12,"sasasa",396565);
+            m1 = (Manager)formatter.Deserialize(fs);
+            Console.WriteLine($"-----Printing DeSerialized Manager Object-----\n{m1}");
+            fs.Close();
+        }
+        public static void Main(String [] args)
+        {
+            Employee emp = new Employee(100,"Vaish",7000);
+            emp.Display();
+            Manager m1 = new Manager(12, "Hritik", 12000);
+            m1.Display();
+            SerializeObject(m1);
+            DeserializeObject();
+        }
+    }
+}
---
+++ Assignmnet7/CSharpAssignmnet7Q1/CSharpAssignmnet7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+namespace CSharpAssignmnet7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\VGANESHM\source\repos\CSharpAssignmnet7";
+
+            var dirs = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);//for getting directories
+            var files = Directory.GetFiles(rootPath, "*.txt", SearchOption.AllDirectories);//for getting files
+
+            Console.WriteLine("-----Displaying directories-----");
+            foreach (string dir in dirs)
+            {
+                var dirInfo = new DirectoryInfo(dir);
+                Console.WriteLine($"{dirInfo.FullName}");
+            }
+            Console.WriteLine("\n-----Displaying all .txt files-----");
+            foreach (string file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                Console.WriteLine($"{Path.GetFileName(file)} - { fileInfo.Length } bytes");
+            }
+        }
+    }
+}
